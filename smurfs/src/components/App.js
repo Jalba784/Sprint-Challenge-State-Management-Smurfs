7f08ce2cd481997853abcaf6398337adf91f785f
josepaniagua <jpanjr@gmail.com>
2020-03-09 13:43:42 -0700

    created addsmurfform

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { SmurfContext } from "../context/SmurfContext.js";
+import SmurfList from '../components/SmurfList.js';
+
 import axios from "axios";
 
 const App = () => {
@@ -30,6 +32,8 @@ const App = () => {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+          <br/>
+          <SmurfList/>
       </div>
     </SmurfContext.Provider>
   );
--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Smurf = props => {
-  const { singleSmurf } = props;
-  return (
-    <div>
-      <h2>{singleSmurf.name}</h2>
-      <p>Age: {singleSmurf.age}</p>
-      <p>Height: {singleSmurf.height}</p>
-    </div>
-  );
+    const { singleSmurf } = props;
+    return (
+        <div>
+            <h2>{singleSmurf.name}</h2>
+            <p>Age: {singleSmurf.age}</p>
+            <p>Height: {singleSmurf.height}</p>
+        </div>
+    );
 };
 
 export default Smurf;
